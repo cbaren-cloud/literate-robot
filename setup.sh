78b7c65532b42492e14431cdc0c976e25c2105d8
cbaren-cloud <cbaren@ripple.com>
2026-03-03 07:32:02 -0800

    Add setup.sh with initial git commands

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "# literate-robot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cbaren-cloud/literate-robot.git
+git push -u origin main
